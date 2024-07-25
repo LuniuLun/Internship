89f1f9c5e8f16a7cb93ea8d5aa43474920b7fcda
DucVan <nguyenducvan260903@gmail.com>
2024-07-25 18:33:14 +0700

    [ADD] Add render error notification

--- javascript/Practice/src/scripts/objects/notification.js
+++ javascript/Practice/src/scripts/objects/notification.js
@@ -12,13 +12,31 @@ class Notification {
 
   static renderNotification() {
     const message = localStorage.getItem('Message')
-
     if (message) {
-      const messageEle = document.querySelector('.message')
-      const contentMessageEle = document.querySelector('.message__content')
+      const messageEle = document.querySelector('.js-success-message')
+      const contentMessageEle = document.querySelector(
+        '.js-success-content-message',
+      )
       contentMessageEle.textContent = message
       messageEle.classList.add('show')
+      setTimeout(() => {
+        messageEle.classList.remove('show')
+      }, 2000)
+      localStorage.removeItem('Message')
+    }
+  }
 
+  static renderErrorNotification(message) {
+    if (message) {
+      const messageEle = document.querySelector('.js-failed-message')
+      const contentMessageEle = document.querySelector(
+        '.js-failed-content-message',
+      )
+      contentMessageEle.textContent = message
+      messageEle.classList.add('show')
+      setTimeout(() => {
+        messageEle.classList.remove('show')
+      }, 2000)
       localStorage.removeItem('Message')
     }
   }
