cefa20e76591b199bb139237b3add3a046fcb9b0
DucVan <nguyenducvan260903@gmail.com>
2024-08-07 16:59:07 +0700

    [UPDATE] Not show notification in service and return a object after calling API

--- javascript/Practice/src/scripts/services/product.js
+++ javascript/Practice/src/scripts/services/product.js
@@ -1,4 +1,3 @@
-/* eslint-disable consistent-return */
 import Message from '../constants/message'
 import Notification from '../utilities/notification'
 
@@ -23,7 +22,7 @@ class ProductService {
   /**
    * Fetches a list of products with a specified limit.
    * @param {number} limit - The number of products to fetch.
-   * @returns {Promise<Object[]> | null} A promise that resolves to the list of products or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object[], message?: string }>} A promise that resolves to the list of products or an error object.
    */
   async getProduct(limit = 9) {
     try {
@@ -31,19 +30,24 @@ class ProductService {
       calledUrl.searchParams.append('page', 1)
       calledUrl.searchParams.append('limit', limit)
       const response = await fetch(calledUrl)
+
       if (!response.ok) {
-        this.notificationInstance.renderNotification({
+        return {
           status: 'error',
           message: Message.NOT_FOUND,
-        })
-        return null
+        }
       }
-      return response.json()
+
+      const data = await response.json()
+      return {
+        status: 'success',
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 
@@ -52,61 +56,72 @@ class ProductService {
    * @param {string} property - The property to filter by.
    * @param {string} value - The value of the property to filter by.
    * @param {number} limit - The number of products to fetch.
-   * @returns {Promise<Object[]> | null} A promise that resolves to the filtered list of products or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object[], message?: string }>} A promise that resolves to the filtered list of products or an error object.
    */
   async filterProduct(property = '', value = '', limit = 9) {
     try {
       const calledUrl = new URL(this.url)
       calledUrl.searchParams.append('page', 1)
       calledUrl.searchParams.append('limit', limit)
+
       if (property !== '' && value !== '') {
         calledUrl.searchParams.append(property, value)
       }
+
       const response = await fetch(calledUrl)
+
       if (!response.ok) {
-        this.notificationInstance.renderNotification({
+        return {
           status: 'error',
           message: Message.NOT_FOUND,
-        })
-        return null
+        }
       }
-      return response.json()
+      const data = await response.json()
+      return {
+        status: 'success',
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 
   /**
    * Fetches a product by its ID.
    * @param {string} id - The ID of the product to fetch.
-   * @returns {Promise<Object | null>} A promise that resolves to the product data or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object, message?: string }>} A promise that resolves to the product data or an error object.
    */
   async getProductById(id) {
     try {
       const response = await fetch(`${this.url}/${id}`)
+
       if (!response.ok) {
-        this.notificationInstance.renderNotification({
+        return {
           status: 'error',
           message: Message.NOT_FOUND,
-        })
-        return null
+        }
       }
-      return response.json()
+
+      const data = await response.json()
+      return {
+        status: 'success',
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 
   /**
    * Adds a new product.
    * @param {Object} newProduct - The new product data to be added.
-   * @returns {Promise<Object | null>} A promise that resolves to the added product data or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object, message?: string }>} A promise that resolves to the added product data or an error object.
    */
   async addProduct(newProduct) {
     try {
@@ -115,26 +130,32 @@ class ProductService {
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify(newProduct),
       })
+
       if (!response.ok) {
-        this.notificationInstance.renderNotification({
+        return {
           status: 'error',
           message: Message.ADD_PRODUCT_FAILED,
-        })
-        return null
+        }
       }
-      return response.json()
+
+      const data = await response.json()
+      return {
+        status: 'success',
+        message: Message.ADD_PRODUCT_SUCCESS,
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 
   /**
    * Edits an existing product.
    * @param {Object} newProduct - The updated product data.
-   * @returns {Promise<Object | null>} A promise that resolves to the updated product data or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object, message?: string }>} A promise that resolves to the updated product data or an error object.
    */
   async editProduct(newProduct) {
     try {
@@ -143,44 +164,57 @@ class ProductService {
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify(newProduct),
       })
+
       if (!response.ok) {
-        this.notificationInstance.renderNotification({
+        return {
           status: 'error',
           message: Message.EDIT_PRODUCT_FAILED,
-        })
-        return null
+        }
       }
-      return response.json()
+
+      const data = await response.json()
+      return {
+        status: 'success',
+        message: Message.EDIT_PRODUCT_SUCCESS,
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 
   /**
    * Deletes a product by its ID.
    * @param {string} id - The ID of the product to delete.
-   * @returns {Promise<Object | null>} A promise that resolves to the deleted product data or null if an error occurs.
+   * @returns {Promise<{ status: string, data?: Object, message?: string }>} A promise that resolves to the deleted product data or an error object.
    */
   async deleteProduct(id) {
     try {
       const response = await fetch(`${this.url}/${id}`, {
         method: 'DELETE',
       })
+
       if (!response.ok) {
-        this.notificationInstance.renderErrorNotification(
-          Message.DELETE_PRODUCT_FAILED,
-        )
-        return null
+        return {
+          status: 'error',
+          message: Message.DELETE_PRODUCT_FAILED,
+        }
       }
-      return response.json()
+
+      const data = await response.json()
+      return {
+        status: 'success',
+        message: Message.DELETE_PRODUCT_SUCCESS,
+        data,
+      }
     } catch (error) {
-      this.notificationInstance.renderNotification({
+      return {
         status: 'error',
-        message: Message.ERROR_NETWORK,
-      })
+        message: error.message,
+      }
     }
   }
 }
