ca897811ac1f186592a4f00a04953040338cbda9
DucVan <nguyenducvan260903@gmail.com>
2024-08-14 14:31:44 +0700

    [FIX] Update REAL_NUMBER regex to allow negative real numbers

--- javascript/Practice/src/scripts/constants/regExp.js
+++ javascript/Practice/src/scripts/constants/regExp.js
@@ -4,7 +4,7 @@ const REGEXP = {
   SPECIAL_CHARS: /[!@#$%^&*()+\-=[\]{};':"\\|,.<>/?]/,
   ALPHANUMERIC: /^[a-zA-Z0-9\s]*$/,
   IMAGE_EXTENSIONS: ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
-  REAL_NUMBER: /^[0-9]*\.?[0-9]*$/,
+  REAL_NUMBER: /^-?[0-9]*\.?[0-9]+$/,
 }
 
 export default REGEXP
