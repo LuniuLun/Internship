1ce73ab7c8c6350711e2d0e63b0dd140596fb611
DucVan <nguyenducvan260903@gmail.com>
2024-09-04 17:09:48 +0700

    [UPDATE] Remove singleton class and apply typescript into sort function

---
+++ typescript/Practice/src/scripts/utilities/sort.ts
@@ -0,0 +1,14 @@
+// Todo: Sorts an array of objects by a specified property in ascending order.
+function SortObjectsByPropertyAZ<T>(array: T[], property: keyof T): T[] {
+  return array.sort((a, b) => {
+    if (a[property] > b[property]) {
+      return 1
+    }
+    if (a[property] < b[property]) {
+      return -1
+    }
+    return 0
+  })
+}
+
+export default SortObjectsByPropertyAZ
