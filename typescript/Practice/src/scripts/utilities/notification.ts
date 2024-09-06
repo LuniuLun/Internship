f6a21e3f4e30053eadea75bacfdd22653d54dd24
DucVan <nguyenducvan260903@gmail.com>
2024-09-06 09:03:07 +0700

    [UPDATE] Check arguments before performing notification function

--- typescript/Practice/src/scripts/utilities/notification.ts
+++ typescript/Practice/src/scripts/utilities/notification.ts
@@ -1,6 +1,7 @@
 /* eslint-disable class-methods-use-this */
 import checkIcon from '../../assets/icons/check.svg'
 import closeIcon from '../../assets/icons/close.svg'
+import MESSAGE from '../constants/message'
 import getValidElements from './getValidElement'
 import handleErrors from './handleError'
 
@@ -21,39 +22,40 @@ class Notification {
   }
 
   renderNotification({ status = '', message = '' }) {
-    this.messageEle = handleErrors(() => getValidElements(this.messageEle))
-    if (!this.messageEle) return
+    if (status && message) {
+      this.messageEle = getValidElements(this.messageEle)
+      if (!this.messageEle) return
 
-    const contentMessageEle = handleErrors(() =>
-      getValidElements(this.messageEle!.querySelector('.js-message-content')),
-    )
-    const iconEle = handleErrors(() =>
-      getValidElements(
-        this.messageEle!.querySelector('.message__icon') as HTMLImageElement,
-      ),
-    )
-    if (!contentMessageEle || !iconEle) return
+      const contentMessageEle = getValidElements(
+        this.messageEle!.querySelector('.js-message-content'),
+      )
+      const iconEle = getValidElements(
+        this.messageEle!.querySelector('.message__icon'),
+      )
+      if (!contentMessageEle || !iconEle) return
+      const iconElement = iconEle as HTMLImageElement
+      // Set appropriate class and icon based on the status
 
-    // Set appropriate class and icon based on the status
-    if (status === 'success') {
-      this.messageEle.classList.add('message--success')
-      this.messageEle.classList.remove('message--error')
-      iconEle.src = checkIcon
-      iconEle.alt = 'success'
-    } else if (status === 'error') {
-      this.messageEle.classList.add('message--error')
-      this.messageEle.classList.remove('message--success')
-      iconEle.src = closeIcon
-      iconEle.alt = 'error'
+      if (status === 'success') {
+        this.messageEle.classList.add('message--success')
+        this.messageEle.classList.remove('message--error')
+        iconElement.src = checkIcon
+        iconElement.alt = 'success'
+      } else if (status === 'error') {
+        this.messageEle.classList.add('message--error')
+        this.messageEle.classList.remove('message--success')
+        iconElement.src = closeIcon
+        iconElement.alt = 'error'
+      }
+      // Set the message content
+      contentMessageEle.textContent = message
+
+      // Show the message and hide it after 3 seconds
+      this.messageEle.classList.add('show')
+      setTimeout(() => {
+        this.messageEle?.classList.remove('show')
+      }, 3000)
     }
-    // Set the message content
-    contentMessageEle.textContent = message
-
-    // Show the message and hide it after 3 seconds
-    this.messageEle.classList.add('show')
-    setTimeout(() => {
-      this.messageEle?.classList.remove('show')
-    }, 3000)
   }
 }
 
