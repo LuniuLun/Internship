2a10f3d1fb8d260842b6a9dca15c1480d9b39729
DucVan <nguyenducvan260903@gmail.com>
2024-09-06 15:55:46 +0700

    [ADD] Check element after querying DOM

--- typescript/Practice/src/scripts/utilities/loader.ts
+++ typescript/Practice/src/scripts/utilities/loader.ts
@@ -1,9 +1,14 @@
+import getValidElements from './getValidElement'
+import handleErrors from './handleError'
+
 class Loader {
   private static instance: Loader
   private popupEle: HTMLElement | null
 
   private constructor() {
-    this.popupEle = document.querySelector('.js-wrapper-loader')
+    this.popupEle = handleErrors(() =>
+      getValidElements(document.querySelector('.js-wrapper-loader')),
+    )
   }
 
   public static getInstance(): Loader {
@@ -15,14 +20,14 @@ class Loader {
   }
 
   // Displays the loader by adding a loader element to the popup and making it visible
-  public showLoader(): { error?: string } | undefined {
-    if (!this.popupEle) return { error: 'Loader element not found' }
+  public showLoader() {
+    if (!this.popupEle) return
     this.popupEle.innerHTML = `<div class='loader'></div>`
     this.popupEle.classList.add('show')
   }
 
   // Hides the loader by removing the 'show' class from the popup
-  public hideLoader(): void {
+  public hideLoader() {
     if (!this.popupEle) return
     this.popupEle.classList.remove('show')
   }
