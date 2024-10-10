609fc8638d86f2971d669c7da7060d2d74ea514b
DucVan <nguyenducvan260903@gmail.com>
2024-10-10 20:35:53 +0700

    [ADD] Global styles

---
+++ react/Practice/practice-one/src/components/GlobalStyles/index.tsx
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+import './animations/fadeInOut.css'
+import './animations/ripple.css'
+import './animations/slideDown.css'
+import './normalize.css'
+import './typography.css'
+import './variables.css'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+    line-height: 1.15;
+
+    @media (min-width: 1440px) {
+      font-size: 18px;
+    }
+
+    @media (min-width: 768px){
+      font-size: 20px;
+    }
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    font-family: 'Barlow', sans-serif;
+    font-size: 1rem;
+    -webkit-text-size-adjust: none;
+    text-size-adjust: none;
+  }
+`
+export default GlobalStyle
