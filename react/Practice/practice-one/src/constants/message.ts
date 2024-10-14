70235f0f7a42357cbdce84cbdac341a535614cd3
DucVan <nguyenducvan260903@gmail.com>
2024-10-14 17:03:23 +0700

    [ADD] Message and regExp constant

---
+++ react/Practice/practice-one/src/constants/message.ts
@@ -0,0 +1,17 @@
+const MESSAGE = {
+  ADD_PRODUCT_SUCCESS: 'Add product successfully',
+  ADD_PRODUCT_FAILED: 'Failed to add product',
+  DELETE_PRODUCT_SUCCESS: 'Delete product successfully',
+  DELETE_PRODUCT_FAILED: 'Failed to delete the product',
+  EDIT_PRODUCT_SUCCESS: 'Update product successfully',
+  EDIT_PRODUCT_FAILED: 'Failed to update the product',
+  GET_PRODUCT_FAILED: 'Failed to load products',
+  GET_PRODUCT_SUCCESS: 'Load products successfully',
+  FILTER_PRODUCT_FAILED: 'Get products successfully',
+  ERROR_EVENT: 'Function not supported yet',
+  UNKNOWN_ERROR: 'Unknown error occurred',
+  NOT_FOUND: 'No Results Found',
+  ERROR_NETWORK: 'Error network'
+}
+
+export default MESSAGE
