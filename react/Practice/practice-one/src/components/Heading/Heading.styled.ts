dff5d0153fa5c6d0b8127032253ebdbe2aea3e5b
DucVan <nguyenducvan260903@gmail.com>
2024-10-16 15:01:19 +0700

    [UPDATE] Remove padding and margin in heading

--- react/Practice/practice-one/src/components/Heading/Heading.styled.ts
+++ react/Practice/practice-one/src/components/Heading/Heading.styled.ts
@@ -1,17 +1,10 @@
 import styled from 'styled-components'
 
-export const HeadingStyled = styled.h1<{ bottomBorder?: boolean }>`
-  margin-top: 0;
+export const HeadingStyled = styled.h1`
+  margin: 0;
   width: 100%;
-  padding-bottom: 24px;
   font-size: 1.75rem;
   line-height: 2.45rem;
   font-weight: 600;
   color: var(--white-text-1);
-
-  ${({ bottomBorder }) =>
-    bottomBorder &&
-    `
-    border-bottom: var(--dark-thin-border);
-  `}
 `
--- react/Practice/practice-one/src/components/Heading/index.tsx
+++ react/Practice/practice-one/src/components/Heading/index.tsx
@@ -2,15 +2,10 @@ import { HeadingStyled } from './Heading.styled'
 
 export interface IHeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
   title: string
-  bottomBorder?: boolean
 }
 
-const Heading = ({ title, bottomBorder, ...props }: IHeadingProps) => {
-  return (
-    <HeadingStyled bottomBorder={bottomBorder} {...props}>
-      {title}
-    </HeadingStyled>
-  )
+const Heading = ({ title, ...props }: IHeadingProps) => {
+  return <HeadingStyled {...props}>{title}</HeadingStyled>
 }
 
 export default Heading
