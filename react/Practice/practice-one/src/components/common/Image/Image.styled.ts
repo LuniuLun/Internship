bcd207ecd8feaf2a61793ef2612cc7749921cc0f
DucVan <nguyenducvan260903@gmail.com>
2024-10-30 15:19:07 +0700

    [UPDATE] Change width handling with switch case for size prop

--- react/Practice/practice-one/src/components/common/Image/Image.styled.ts
+++ react/Practice/practice-one/src/components/common/Image/Image.styled.ts
@@ -2,7 +2,16 @@ import styled from 'styled-components'
 import { IImageProps } from '.'
 
 export const ImageStyled = styled.img<IImageProps>`
-  width: ${({ size }) => (size === 'sm' ? '4px' : '127px')};
+  width: ${({ size }) => {
+    switch (size) {
+      case 'tiny':
+        return '4px'
+      case 'sm':
+        return '18px'
+      default:
+        return '127px'
+    }
+  }};
   max-width: 100%;
   border-radius: 50%;
   aspect-ratio: 1;
