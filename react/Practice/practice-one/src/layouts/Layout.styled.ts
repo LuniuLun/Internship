fcbe1db099deba454a60ad2dd6dcf40276d255e9
DucVan <nguyenducvan260903@gmail.com>
2024-10-31 14:43:46 +0700

    [UPDATE] Apply styled component and refactor layout

--- react/Practice/practice-one/src/layouts/Layout.styled.ts
+++ react/Practice/practice-one/src/layouts/Layout.styled.ts
@@ -1,15 +1,13 @@
-html {
-  overflow: hidden;
-}
+import styled from 'styled-components'
 
-.wrapper {
+export const Wrapper = styled.div`
   background-color: var(--dark-bg-1);
   padding: 20px 18px;
   height: 100vh;
   overflow-y: auto;
-}
+`
 
-.content {
+export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -18,4 +16,4 @@ html {
   min-height: 100vh;
   padding: 75px 1.5rem 26px 1.5rem;
   background-color: var(--dark-bg-2);
-}
+`
--- react/Practice/practice-one/src/layouts/index.tsx
+++ react/Practice/practice-one/src/layouts/index.tsx
@@ -1,1 +1,15 @@
-export { default as DefaultLayout } from './DefaultLayout/DefaultLayout'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import { Content, Wrapper } from './Layout.styled'
+
+const DefaultLayout = () => {
+  return (
+    <Wrapper>
+      <Content>
+        <Header />
+        <Outlet />
+      </Content>
+    </Wrapper>
+  )
+}
+export default DefaultLayout
