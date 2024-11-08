680a722f62002623442d91355c201da082187196
DucVan <nguyenducvan260903@gmail.com>
2024-11-08 16:03:20 +0700

    [ADD] Try to config env with jest testing

---
+++ react/Learn/Jest/getting-started/jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    // "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|svg)$":
+    //   "jest-transform-stub",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+};
