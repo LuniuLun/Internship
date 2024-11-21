602291556152bfed5b44d813559125b0a0667a21
DucVan <nguyenducvan260903@gmail.com>
2024-11-21 17:00:16 +0700

    [UPDATE] Replaced local state management with `forwardRef` to support ref forwarding.

--- react/Learn/Tanstack/practice-one/src/components/TextField/index.tsx
+++ react/Learn/Tanstack/practice-one/src/components/TextField/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { forwardRef } from 'react'
 import { TSizeVariant } from '@type/variant'
 import { Input, Wrapper } from './TextField.styled'
 import { Typography } from '../common'
@@ -10,30 +10,20 @@ export interface ITextFieldProps extends React.InputHTMLAttributes<HTMLInputElem
   errorMessage?: string
 }
 
-const TextField = ({ value, name, label, errorMessage, dimension = 'lg', onChange, ...props }: ITextFieldProps) => {
-  const [valueInput, setValueInput] = useState(value)
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValueInput(e.target.value)
-    onChange?.(e)
+const TextField = forwardRef<HTMLInputElement, ITextFieldProps>(
+  ({ name, label, errorMessage, dimension = 'lg', ...props }, ref) => {
+    return (
+      <Wrapper>
+        {label && (
+          <Typography as='label' htmlFor={name} style={{ marginBottom: '8px', color: 'var(--white-text-1)' }}>
+            {label}
+          </Typography>
+        )}
+        <Input ref={ref} name={name} className='normal-thin-text' $dimension={dimension} {...props} />
+        <Typography variant='error-message'>{errorMessage}</Typography>
+      </Wrapper>
+    )
   }
-  return (
-    <Wrapper>
-      {label && (
-        <Typography as='label' htmlFor={name} style={{ marginBottom: '8px', color: 'var(--white-text-1)' }}>
-          {label}
-        </Typography>
-      )}
-      <Input
-        name={name}
-        value={valueInput}
-        className='normal-thin-text'
-        onChange={handleChange}
-        $dimension={dimension}
-        {...props}
-      />
-      <Typography variant='error-message'>{errorMessage || ''}</Typography>
-    </Wrapper>
-  )
-}
+)
 
 export default TextField
