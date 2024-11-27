3360c578195101b97753421519c9d47e3bc640d2
DucVan <nguyenducvan260903@gmail.com>
2024-11-27 15:36:43 +0700

    [ADD] Sample about theme customization

---
+++ react/Learn/ChakraUI/themecustomization/src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const defaultTheme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#845EC2",
+      secondary: "#FF6F91",
+      highlight: "#00C9A7",
+      warning: "#FFC75F",
+      danger: "#C34A36",
+    },
+  },
+});
