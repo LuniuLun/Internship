fec6d648d7be25c742cad7d77150b3f3ea088b31
DucVan <nguyenducvan260903@gmail.com>
2024-11-29 16:42:30 +0700

    [ADD] Configure Vite aliases for cleaner imports

---
+++ react/Practice/practice-two/src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as InfoGroup } from './InfoGroup'
--- react/Practice/practice-two/vite.config.ts
+++ react/Practice/practice-two/vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components')
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@types': path.resolve(__dirname, 'src/types')
     }
   }
 })
