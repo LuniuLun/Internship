544613c712c1ef5715e1f5e3f6e2c970ce830289
DucVan <nguyenducvan260903@gmail.com>
2024-11-29 16:41:02 +0700

    [ADD] Information group component

---
+++ react/Practice/practice-two/src/components/InfoGroup/index.tsx
@@ -0,0 +1,20 @@
+import { memo } from 'react'
+import { Description, Heading, InfoGroupStyled } from './InfoGroup'
+import { TSizeInfoGroup } from '@type/variant'
+
+interface IInfoGroupProps {
+  heading: string
+  description: string
+  size: TSizeInfoGroup
+}
+
+const InfoGroup = ({ heading, description, size = 'sm' }: IInfoGroupProps) => {
+  return (
+    <InfoGroupStyled>
+      <Heading $size={size}>{heading}</Heading>
+      <Description $size={size}>{description}</Description>
+    </InfoGroupStyled>
+  )
+}
+
+export default memo(InfoGroup)
