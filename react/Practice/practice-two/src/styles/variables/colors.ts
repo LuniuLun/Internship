97a738f29d7c7913a999aa6d9e725cf1fc4cf9d1
DucVan <nguyenducvan260903@gmail.com>
2024-11-29 14:06:22 +0700

    [ADD] Color variables

---
+++ react/Practice/practice-two/src/styles/variables/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+  brand: {
+    primary: '#4A85F6', // --primary-color
+    secondary: '#8F9BB3', // --secondary-color
+    white: '#FFFFFF', // --white-text-default
+    black: '#000000', // --black-text-default
+    blackTextPrimary: '#222B45', // --black-text-primary
+    blackTextSecondary: '#8F9BB3', // --black-text-secondary
+    blackTextTertiary: '#757575', // --black-text-tertiary
+    whiteBg: '#FFFFFF', // --white-bg
+    blackBg: '#09090B', // --black-bg
+    greyBg: '#192038', // --grey-bg
+    hoverBtnColor: '#0095FF' // --hover-btn-color
+  }
+}
+
+export default colors
