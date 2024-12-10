5e5ba3e7121002f4d6303775e5fc6fa0da33b907
DucVan <nguyenducvan260903@gmail.com>
2024-12-11 00:04:12 +0700

    [ADD] Storybook with Chakra UI for InfoGroup component

---
+++ react/Practice/practice-two/src/components/InfoGroup/InfoGroup.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import InfoGroup from '.'
+
+const meta = {
+  title: 'Components/InfoGroup',
+  component: InfoGroup,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: { type: 'radio', options: ['sm', 'lg'] }
+    }
+  },
+  args: {
+    heading: 'Sample Heading',
+    description: 'This is a sample description for InfoGroup component.',
+    size: 'sm'
+  }
+} satisfies Meta<typeof InfoGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    heading: 'Sample Heading',
+    description: 'This is a sample description for InfoGroup component.',
+    size: 'sm'
+  }
+}
+
+export const Large: Story = {
+  args: {
+    heading: 'Sample Heading (Large)',
+    description: 'This is a sample description for a middle-sized InfoGroup component.',
+    size: 'md'
+  }
+}
