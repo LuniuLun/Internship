8e32bcc5b60eb6aa15115ce27f5ed45ecb32450d
DucVan <nguyenducvan260903@gmail.com>
2024-12-11 00:03:51 +0700

    [ADD] Configure Storybook with Chakra UI and Vite support

--- react/Practice/practice-two/.storybook/main.ts
+++ react/Practice/practice-two/.storybook/main.ts
@@ -1,12 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon'
   ],
   framework: {
     name: '@storybook/react-vite',
