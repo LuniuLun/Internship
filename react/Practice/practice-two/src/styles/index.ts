1b1b3c9f90c88bc5e4d61fb511756828f5317aae
DucVan <nguyenducvan260903@gmail.com>
2024-12-13 14:17:15 +0700

    [DELETE] Radii variables

--- react/Practice/practice-two/src/styles/index.ts
+++ react/Practice/practice-two/src/styles/index.ts
@@ -1,12 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 import colors from './variables/colors'
-import radii from './variables/radii'
 import globalStyles from './globalStyles'
 import { ButtonStyles as Button, HeadingStyles as Heading, InputStyles as Input } from './components'
 
 export const defaultTheme = extendTheme({
   colors,
-  radii,
   styles: globalStyles.styles,
   config: globalStyles.config,
   fonts: globalStyles.fonts,
--- react/Practice/practice-two/src/styles/variables/radii.ts
+++
@@ -1,6 +0,0 @@
-const radii = {
-  md: '8px', // --border-radius-md
-  lg: '16px' // --border-radius-lg
-}
-
-export default radii
