8d69534fe44be94ca4490fd0ee716fb7dc85eedf
DucVan <nguyenducvan260903@gmail.com>
2024-12-13 16:45:25 +0700

    [UPDATE] Change table component UI and apply CustomCell component into it

--- react/Practice/practice-two/src/components/CustomTable/index.tsx
+++ react/Practice/practice-two/src/components/CustomTable/index.tsx
@@ -1,6 +1,7 @@
-import { Table, Thead, Tbody, Tr, Th, Td, IconButton, Flex, Button, TableCaption } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td, IconButton, Flex, TableCaption } from '@chakra-ui/react'
 import { BinIcon, PenIcon } from '@assets/icons'
 import colors from '@styles/variables/colors'
+import CustomCell from './CustomCell'
 
 export interface TableRow {
   [key: string]: string | number | boolean | React.ReactNode
@@ -18,17 +19,19 @@ const CustomTable = ({ title, data, onEdit, onDelete }: CustomTableProps) => {
 
   return (
     <Table>
-      <TableCaption
-        placement='top'
-        marginTop={0}
-        padding={5}
-        textAlign={'left'}
-        color={colors.brand.blackTextPrimary}
-        fontSize={'1.0625rem'}
-        fontWeight={'bold'}
-      >
-        {title}
-      </TableCaption>
+      {title && (
+        <TableCaption
+          placement='top'
+          marginTop={0}
+          padding={5}
+          textAlign='left'
+          color={colors.brand.blackTextPrimary}
+          fontSize='1.0625rem'
+          fontWeight='bold'
+        >
+          {title}
+        </TableCaption>
+      )}
       <Thead>
         <Tr bgColor={colors.brand.secondary} color={colors.brand.blackTextSecondary}>
           {headers.map((header, index) => (
@@ -36,50 +39,42 @@ const CustomTable = ({ title, data, onEdit, onDelete }: CustomTableProps) => {
               key={header}
               borderBottom={`2px solid ${colors.brand.secondary}`}
               padding={5}
+              fontSize='md'
               textAlign={index === 0 ? 'left' : 'center'}
             >
               {header}
             </Th>
           ))}
-          <Th textAlign={'center'} w={'100px'} padding={5} borderBottom={`2px solid ${colors.brand.secondary}`}>
+          <Th textAlign='center' w='100px' padding={5} borderBottom={`2px solid ${colors.brand.secondary}`}>
             Action
           </Th>
         </Tr>
       </Thead>
       <Tbody>
         {data.map((row, rowIndex) => (
-          <Tr key={rowIndex}>
+          <Tr key={rowIndex} fontSize='sm' color={colors.brand.blackTextPrimary}>
             {headers.map((header, index) => (
               <Td
                 key={header}
                 borderBottom={`2px solid ${colors.brand.secondary}`}
                 textAlign={index === 0 ? 'left' : 'center'}
+                bgColor={colors.brand.white}
               >
-                {header.toLowerCase() === 'role' ? (
-                  <Button
-                    w='111px'
-                    size='sm'
-                    variant={row[header]?.toString().toLowerCase().includes('admin') ? 'primary' : 'secondary'}
-                  >
-                    {row[header]}
-                  </Button>
-                ) : (
-                  row[header]
-                )}
+                {CustomCell({ header, row })}
               </Td>
             ))}
-            <Td w={'100px'} borderBottom={`2px solid ${colors.brand.secondary}`} bgColor={colors.brand.white}>
+            <Td w='100px' borderBottom={`2px solid ${colors.brand.secondary}`} bgColor={colors.brand.white}>
               <Flex gap={2}>
                 <IconButton
                   aria-label='Edit'
-                  bgColor={'transparent'}
+                  bgColor={colors.brand.white}
                   icon={<PenIcon />}
                   size='sm'
                   onClick={() => onEdit(row)}
                 />
                 <IconButton
                   aria-label='Delete'
-                  bgColor={'transparent'}
+                  bgColor={colors.brand.white}
                   icon={<BinIcon />}
                   size='sm'
                   colorScheme='red'
