7b82589f4f0a7400622e8b0031b073d13eda7a7e
DucVan <nguyenducvan260903@gmail.com>
2024-12-16 15:21:18 +0700

    [ADD] Header component

---
+++ react/Practice/practice-two/src/layout/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { NotificationIcon } from '@assets/icons'
+import { Box, Flex } from '@chakra-ui/react'
+import { InfoGroup, UserCard } from '@components'
+
+const Header = () => {
+  return (
+    <Flex alignItems='center' justifyContent='space-between' w='100%' padding='31px 0 26px 13px' bgColor='transparent'>
+      <InfoGroup heading='Hello, Lekan' description='Have a nice day' size='md' />
+      <Flex
+        alignItems='center'
+        gap={5}
+        sx={{ '> *:not(:first-of-type)': { borderLeft: '1px solid #ccc', paddingLeft: 5 } }}
+      >
+        <Box height='100%'>
+          <NotificationIcon />
+        </Box>
+        <UserCard name='Lekan Okeowo' role='Admin' avatar='' />
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Header
