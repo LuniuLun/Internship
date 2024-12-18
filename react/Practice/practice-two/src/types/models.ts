fe3c394c7ed8aefe20da89efe864934c08adc339
DucVan <nguyenducvan260903@gmail.com>
2024-12-18 15:53:16 +0700

    [ADD] Add IUser interface definition for user data structure

--- react/Practice/practice-two/src/types/models.ts
+++ react/Practice/practice-two/src/types/models.ts
@@ -1,6 +1,11 @@
-export interface User {
-  name: string
+export interface IUser {
+  id: string
+  firstName: string
+  lastName: string
   email: string
+  phone: string
+  username: string
+  password: string
   role: string
   createDate: string
 }
