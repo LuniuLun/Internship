0596eeccb6984ddade718a1aca6e55f29d3f3d39
DucVan <nguyenducvan260903@gmail.com>
2024-12-18 15:51:32 +0700

    [ADD] Sort and pagination options for CustomSelect component

---
+++ react/Practice/practice-two/src/constants/option.ts
@@ -0,0 +1,8 @@
+import { SelectOption } from '@components/CustomSelect'
+
+export const sortOptions: SelectOption<string>[] = [
+  { value: 'name', label: 'Name' },
+  { value: 'role', label: 'Role' }
+]
+
+export const itemsPerPageOptions = [5, 10, 15, 20, 50]
