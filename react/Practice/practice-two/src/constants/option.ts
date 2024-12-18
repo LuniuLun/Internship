c0925a6cb3369e7145807964a473001be1af743c
DucVan <nguyenducvan260903@gmail.com>
2024-12-18 21:03:26 +0700

    [UPDATE] Change value of option sort by name and add sort by createDate

--- react/Practice/practice-two/src/constants/option.ts
+++ react/Practice/practice-two/src/constants/option.ts
@@ -1,8 +1,9 @@
 import { SelectOption } from '@components/CustomSelect'
 
 export const sortOptions: SelectOption<string>[] = [
-  { value: 'name', label: 'Name' },
-  { value: 'role', label: 'Role' }
+  { value: 'firstName', label: 'Name' },
+  { value: 'role', label: 'Role' },
+  { value: 'createDate', label: 'Create date' }
 ]
 
 export const itemsPerPageOptions = [5, 10, 15, 20, 50]
