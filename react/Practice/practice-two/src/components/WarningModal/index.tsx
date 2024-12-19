483d4d5e2de59104bb6269b7818a35a82f500b2f
DucVan <nguyenducvan260903@gmail.com>
2024-12-19 17:03:34 +0700

    [ADD] Warning component

---
+++ react/Practice/practice-two/src/components/WarningModal/index.tsx
@@ -0,0 +1,24 @@
+import { Box, Text } from '@chakra-ui/react'
+import { CustomModal } from '@components'
+
+interface WarningModalProps {
+  isModalOpen: boolean
+  onClose: () => void
+  title: string
+  message: string
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+}
+
+const WarningModal = ({ isModalOpen, onClose, title, message, handleSubmit }: WarningModalProps) => {
+  return (
+    <CustomModal size={'lg'} isOpen={isModalOpen} onClose={onClose} title={title} handleSubmit={handleSubmit}>
+      <Box textAlign='center' marginTop={4} px={10}>
+        <Text fontSize='lg' color='brand.red' fontWeight='Bold'>
+          {message}
+        </Text>
+      </Box>
+    </CustomModal>
+  )
+}
+
+export default WarningModal
