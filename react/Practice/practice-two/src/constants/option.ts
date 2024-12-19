50f2910f3a77e5d5f5ef71bb5e7495a060682f75
DucVan <nguyenducvan260903@gmail.com>
2024-12-19 15:58:59 +0700

    [ADD] Add module permission and uppercase for variables

--- react/Practice/practice-two/src/constants/option.ts
+++ react/Practice/practice-two/src/constants/option.ts
@@ -1,9 +1,36 @@
 import { SelectOption } from '@components/CustomSelect'
 
-export const sortOptions: SelectOption<string>[] = [
+export const SORT_OPTION: SelectOption<string>[] = [
   { value: 'firstName', label: 'Name' },
   { value: 'role', label: 'Role' },
   { value: 'createDate', label: 'Create date' }
 ]
 
-export const itemsPerPageOptions = [5, 10, 15, 20, 50]
+export const ROLE_OPTION: SelectOption<string>[] = [
+  { value: 'Super Admin', label: 'Super Admin' },
+  { value: 'Admin', label: 'Admin' },
+  { value: 'Employee', label: 'Employee' }
+]
+
+export const MODULE_PERMISSION = [
+  {
+    modulePermission: 'Super Admin',
+    read: true,
+    write: true,
+    delete: true
+  },
+  {
+    modulePermission: 'Admin',
+    read: true,
+    write: false,
+    delete: false
+  },
+  {
+    modulePermission: 'Employee',
+    read: true,
+    write: false,
+    delete: false
+  }
+]
+
+export const ITEM_PER_PAGE = [5, 10, 15, 20, 50]
