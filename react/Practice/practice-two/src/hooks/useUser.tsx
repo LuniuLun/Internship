c2cc83ea6efe82104938dfe79d32265a93907d2f
DucVan <nguyenducvan260903@gmail.com>
2024-12-20 17:19:37 +0700

    [UPDATE] Refactor useUser hook for improved data handling

--- react/Practice/practice-two/src/hooks/useUser.tsx
+++ react/Practice/practice-two/src/hooks/useUser.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import InfoGroup from '@components/InfoGroup'
 import { TableRow } from '@components/CustomTable'
 import { IUser } from '@type/models'
-import { addUser, deleteUser, editUser, fetchAllUsers } from '@services/user'
+import { addUser, deleteUser, editUser } from '@services/user'
 import { useMutation, UseMutationResult, useQueryClient } from '@tanstack/react-query'
 import { IApiResponse } from '@type/apiResponse'
 
@@ -11,31 +11,21 @@ interface TransformedUser extends Pick<IUser, 'id' | 'role' | 'createDate'>, Tab
 }
 
 interface UseUserReturn {
-  loading: boolean
-  error: string
-  allUsers: IUser[]
   superAdmin: IUser[]
   admin: IUser[]
   employee: IUser[]
-  getAllUser: () => void
   transformedUsers: TransformedUser[]
-  transformFullUsers: TableRow[]
+  transformAllUsers: TableRow[]
   addUserMutation: UseMutationResult<IApiResponse<IUser>, Error, IUser>
   editUserMutation: UseMutationResult<IApiResponse<IUser>, Error, IUser>
   deleteUserMutation: UseMutationResult<IApiResponse<IUser>, Error, IUser>
 }
 
-export const useUser = (users: IUser[]): UseUserReturn => {
-  const [allUsers, setAllUsers] = useState<IUser[]>([])
-  const [superAdmin, setSuperAdmin] = useState<IUser[]>([])
-  const [admin, setAdmin] = useState<IUser[]>([])
-  const [employee, setEmployee] = useState<IUser[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
+export const useUser = (usersData: IUser[], allUsers: IUser[]): UseUserReturn => {
   const queryClient = useQueryClient()
 
   const transformedUsers = useMemo(() => {
-    return users.map((user) => {
+    return usersData.map((user) => {
       return {
         id: user.id,
         name: <InfoGroup heading={`${user.firstName} ${user.lastName}`} description={user.email} size='sm' />,
@@ -43,10 +33,10 @@ export const useUser = (users: IUser[]): UseUserReturn => {
         createDate: user.createDate.split('T')[0]
       }
     })
-  }, [users])
+  }, [usersData])
 
-  const transformFullUsers = useMemo((): TableRow[] => {
-    return users.map((user) => ({
+  const transformAllUsers = useMemo((): TableRow[] => {
+    return usersData.map((user) => ({
       id: user.id,
       lastName: user.lastName,
       firstName: user.firstName,
@@ -57,27 +47,14 @@ export const useUser = (users: IUser[]): UseUserReturn => {
       role: user.role,
       createDate: user.createDate.split('T')[0]
     }))
-  }, [users])
+  }, [usersData])
 
-  const getAllUser = () => {
-    const getAllUser = async () => {
-      setLoading(true)
-      const response = await fetchAllUsers()
-
-      if (response.status === 'success' && response.data) {
-        setAllUsers(response.data)
-        setSuperAdmin(response.data.filter((user) => user.role === 'Super Admin'))
-        setAdmin(response.data.filter((user) => user.role === 'Admin'))
-        setEmployee(response.data.filter((user) => user.role === 'Employee'))
-      } else {
-        setError(response.message)
-      }
-
-      setLoading(false)
-    }
-
-    getAllUser()
-  }
+  const { superAdmin, admin, employee } = useMemo(() => {
+    const superAdminUsers = allUsers.filter((user) => user.role === 'Super Admin')
+    const adminUsers = allUsers.filter((user) => user.role === 'Admin')
+    const employeeUsers = allUsers.filter((user) => user.role === 'Employee')
+    return { superAdmin: superAdminUsers, admin: adminUsers, employee: employeeUsers }
+  }, [allUsers])
 
   const addUserMutation = useMutation<IApiResponse<IUser>, Error, IUser>({
     mutationFn: addUser,
@@ -101,15 +78,11 @@ export const useUser = (users: IUser[]): UseUserReturn => {
   })
 
   return {
-    error,
-    loading,
-    allUsers,
     superAdmin,
     admin,
     employee,
     transformedUsers,
-    transformFullUsers,
-    getAllUser,
+    transformAllUsers,
     addUserMutation,
     editUserMutation,
     deleteUserMutation
