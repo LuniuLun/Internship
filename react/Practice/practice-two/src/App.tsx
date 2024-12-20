995f5fb8bca8584d649f68d563bb10e8c9eb2a1a
DucVan <nguyenducvan260903@gmail.com>
2024-12-20 17:19:07 +0700

    [ADD] Add Users page route to the app

--- react/Practice/practice-two/src/App.tsx
+++ react/Practice/practice-two/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import DefaultLayout from '@layout'
 import Dashboard from '@pages/Dashboard'
+import Users from '@pages/Users'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route index element={<Dashboard />} />
+        <Route path='users' element={<Users />} />
       </Route>
     </Routes>
   )
