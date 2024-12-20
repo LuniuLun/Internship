d2cf26aa7a9e705613061134e2746dac0b8b5d70
DucVan <nguyenducvan260903@gmail.com>
2024-12-20 15:18:15 +0700

    [ADD] Dynamically generate sidebar items and icons from NAV_ITEMS configuration

--- react/Practice/practice-two/src/layout/components/Sidebar/index.tsx
+++ react/Practice/practice-two/src/layout/components/Sidebar/index.tsx
@@ -1,32 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { Box, Stack } from '@chakra-ui/react'
+import { Logo, NavItem } from '@components'
+import { useLocation } from 'react-router-dom'
+import { NAV_ITEMS } from '@constants/option'
 import {
   DashboardIcon,
+  LeaderboardIcon,
   DocumentIcon,
   GalleryIcon,
   HelpIcon,
   HierarchyIcon,
-  LeaderboardIcon,
-  LogoIcon,
   MessageIcon,
-  SettingIcon
+  SettingIcon,
+  LogoIcon
 } from '@assets/icons'
-import { Box, Stack } from '@chakra-ui/react'
-import { Logo, NavItem } from '@components'
+
+const iconMap: Record<string, React.ElementType> = {
+  dashboard: DashboardIcon,
+  users: LeaderboardIcon,
+  documents: DocumentIcon,
+  photos: GalleryIcon,
+  hierarchy: HierarchyIcon,
+  message: MessageIcon,
+  help: HelpIcon,
+  setting: SettingIcon
+}
 
 const Sidebar = () => {
+  const location = useLocation()
+  const [activeNavItem, setActiveNavItem] = useState<string>('dashboard')
+
+  useEffect(() => {
+    const currentPath = location.pathname
+    const normalizedCurrentPath = currentPath.replace(/\/$/, '')
+
+    const matchedItem = NAV_ITEMS.find((item) => {
+      return normalizedCurrentPath === item.path || normalizedCurrentPath.startsWith(item.path + '/')
+    })
+
+    setActiveNavItem(matchedItem ? matchedItem.id : 'dashboard')
+  }, [location.pathname])
+
   return (
     <Stack gap={10} maxW='254px' w='100%' height='100vh' padding='40px 0 40px'>
       <Box paddingLeft='32px'>
-        <Logo icon={<LogoIcon />} src={'/'} />
+        <Logo icon={<LogoIcon />} src='/' />
       </Box>
       <Stack gap={2}>
-        <NavItem icon={<DashboardIcon />} title='Dashboard' isActive={true} handleClick={() => {}} />
-        <NavItem icon={<LeaderboardIcon />} title='Users' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<DocumentIcon />} title='Documents' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<GalleryIcon />} title='Photos' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<HierarchyIcon />} title='Hierarchy' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<MessageIcon />} title='Message' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<HelpIcon />} title='Help' isActive={false} handleClick={() => {}} />
-        <NavItem icon={<SettingIcon />} title='Setting' isActive={false} handleClick={() => {}} />
+        {NAV_ITEMS.map((item) => {
+          const Icon = iconMap[item.id] || DashboardIcon
+          return (
+            <NavItem
+              key={item.id}
+              icon={<Icon />}
+              title={item.title}
+              isActive={activeNavItem === item.id}
+              to={item.path}
+            />
+          )
+        })}
       </Stack>
     </Stack>
   )
