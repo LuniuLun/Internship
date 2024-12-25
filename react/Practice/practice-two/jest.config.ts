814bea00400ad9eb5e34f395c264990e8e13c95e
DucVan <nguyenducvan260903@gmail.com>
2024-12-26 00:14:03 +0700

    [UPDATE] Update Jest configuration with additional module mappings

--- react/Practice/practice-two/jest.config.ts
+++ react/Practice/practice-two/jest.config.ts
@@ -6,10 +6,14 @@ export default {
   },
   moduleNameMapper: {
     '^@components(.*)$': '<rootDir>/src/components$1',
-    '^@assets/icons(.*)$': '<rootDir>/src/assets/icons$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
     '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    '^@type/(.*)$': '<rootDir>/src/types/$1'
+    '^@type/(.*)$': '<rootDir>/src/types/$1',
+    '^@layout(.*)$': '<rootDir>/src/layout$1',
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@constants(.*)$': '<rootDir>/src/constants$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^@services(.*)$': '<rootDir>/src/services$1'
   },
   moduleDirectories: ['node_modules', 'src']
 }
