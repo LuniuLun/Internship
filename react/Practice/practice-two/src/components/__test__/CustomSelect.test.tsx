b633f7a647bfd3164575ae6ffc9238b545192a23
DucVan <nguyenducvan260903@gmail.com>
2024-12-26 00:13:29 +0700

    [UPDATE] Enhance tests for CustomSelect component

--- react/Practice/practice-two/src/components/__test__/CustomSelect.test.tsx
+++ react/Practice/practice-two/src/components/__test__/CustomSelect.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen, fireEvent } from '@testing-library/react'
 import CustomSelect from '@components/CustomSelect'
+import colors from '@styles/variables/colors'
 
 const mockOnChange = jest.fn()
 
@@ -28,23 +29,38 @@ describe('CustomSelect', () => {
     const selectElement = screen.getByRole('combobox')
     fireEvent.change(selectElement, { target: { value: 'option1' } })
 
-    expect(mockOnChange).toHaveBeenCalledWith('option1')
+    expect(mockOnChange).toHaveBeenCalledWith(
+      expect.objectContaining({
+        target: expect.objectContaining({ value: 'option1' })
+      })
+    )
   })
 
   it('applies correct border variant based on the border prop', () => {
     const { rerender } = render(<CustomSelect options={options} border='bottom' />)
 
     const selectElement = screen.getByRole('combobox')
-    expect(selectElement).toHaveStyle('border-bottom: 1px solid black')
+    expect(selectElement).toHaveStyle(`border-bottom: 1px solid ${colors.brand.black}`)
 
     rerender(<CustomSelect options={options} border='none' />)
-    expect(selectElement).not.toHaveStyle('border-bottom: 1px solid black')
+    expect(selectElement).not.toHaveStyle(`border-bottom: 1px solid ${colors.brand.black}`)
+    expect(selectElement).not.toHaveStyle(`border: 2px solid ${colors.brand.secondary}`)
+
+    rerender(<CustomSelect options={options} border='full' />)
+    expect(selectElement).toHaveStyle(`border: 2px solid ${colors.brand.secondary}`)
   })
 
   it('renders the default placeholder if none is provided', () => {
     render(<CustomSelect options={options} />)
 
     const selectElement = screen.getByRole('combobox')
-    expect(selectElement).toHaveTextContent('Select an option')
+    expect(selectElement).toHaveTextContent('Select')
+  })
+
+  it('sets selected value when value prop is passed', () => {
+    render(<CustomSelect options={options} value='option2' />)
+
+    const selectElement = screen.getByRole('combobox')
+    expect(selectElement).toHaveValue('option2')
   })
 })
