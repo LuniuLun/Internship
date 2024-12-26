b88c1d9d615b88940ffd46fad51c59070da269e5
DucVan <nguyenducvan260903@gmail.com>
2024-12-26 22:18:17 +0700

    [ADD] Add FilterProvider and update QueryClient configuration with retry options

--- react/Practice/practice-two/src/main.tsx
+++ react/Practice/practice-two/src/main.tsx
@@ -9,16 +9,25 @@ import { defaultTheme } from './styles'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { FilterProvider } from '@components/Filter/FilterProvider'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={defaultTheme} resetCSS>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
+        <FilterProvider>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </FilterProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
