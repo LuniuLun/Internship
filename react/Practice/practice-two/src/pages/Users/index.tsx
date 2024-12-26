19095ff1f4d70e53586c087b05efc583fd57f707
DucVan <nguyenducvan260903@gmail.com>
2024-12-26 22:15:14 +0700

    [UPDATE] Refactor Users page to use FilterContext for filtering users

--- react/Practice/practice-two/src/pages/Users/index.tsx
+++ react/Practice/practice-two/src/pages/Users/index.tsx
@@ -1,73 +1,64 @@
-import { useEffect, useState } from 'react'
 import { Button, Flex, Heading, Stack, useDisclosure } from '@chakra-ui/react'
-import { FilterIcon, PlusIcon, SearchIcon } from '@assets/icons'
-import { CustomTable, TextField, Pagination, CustomSelect, UserModal, WarningModal, StatisticCard } from '@components'
+import { PlusIcon } from '@assets/icons'
+import { CustomTable, Pagination, UserModal, WarningModal, StatisticCard, Filter } from '@components'
 import { useUser } from '@hooks/useUser'
 import { IUser } from '@type/models'
 import { fetchAllUsers, fetchUsers } from '@services/user'
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
-import { ITEM_PER_PAGE, SORT_OPTION } from '@constants/option'
+import { ITEM_PER_PAGE } from '@constants/option'
 import { useCustomToast } from '@hooks/useCustomToast'
+import { useFilter } from '@hooks/useFilter'
+import { useEffect, useState } from 'react'
 
 const Dashboard = () => {
   const { showToast } = useCustomToast()
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const [sortBy, setSortBy] = useState<string>('')
+  const { searchQuery, sortBy } = useFilter()
   const [currentPage, setCurrentPage] = useState<number>(0)
   const [itemsPerPage, setItemsPerPage] = useState<number>(5)
   const [selectedUser, setSelectedUser] = useState<IUser | null>(null)
   const { isOpen: isUserModalOpen, onOpen: onOpenUserModal, onClose: onCloseUserModal } = useDisclosure()
   const { isOpen: isWarningModalOpen, onOpen: onOpenWarningModal, onClose: onCloseWarningModal } = useDisclosure()
 
-  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, refetch, error } = useInfiniteQuery({
-    queryKey: ['users', itemsPerPage, searchQuery, sortBy],
-    queryFn: async ({ pageParam = 1 }) => {
-      return await fetchUsers({
-        page: pageParam.toString(),
-        limit: itemsPerPage.toString(),
-        property: 'firstName',
-        value: searchQuery,
-        sortBy,
-        order: 'asc'
-      })
-    },
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, _, lastPageParam) => {
-      if (!lastPage.data || lastPage.data.length === 0) return undefined
-      return lastPageParam + 1
-    },
-    staleTime: 5 * 60 * 1000
-  })
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, refetch, isError, error } =
+    useInfiniteQuery({
+      queryKey: ['users', itemsPerPage, searchQuery, sortBy],
+      queryFn: async ({ pageParam = 1 }) => {
+        return await fetchUsers({
+          page: pageParam.toString(),
+          limit: itemsPerPage.toString(),
+          property: 'firstName',
+          value: searchQuery,
+          sortBy,
+          order: 'asc'
+        })
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, _, lastPageParam) => {
+        if (!lastPage.data || lastPage.data.length === 0) return undefined
+        return lastPageParam + 1
+      },
+      staleTime: 5 * 60 * 1000
+    })
 
   const {
     data: allUsers,
     isLoading,
+    isError: allUsersIsError,
     error: allUsersError
   } = useQuery({
-    queryKey: ['allUsers'],
-    queryFn: async () => {
-      const response = await fetchAllUsers()
-      return response.data
-    },
+    queryKey: ['allUsers', searchQuery],
+    queryFn: () => fetchAllUsers('firstName', searchQuery),
     staleTime: 5 * 60 * 1000
   })
 
   const usersData: IUser[] = data?.pages[currentPage]?.data || []
   const { transformAllUsers, addUserMutation, editUserMutation, deleteUserMutation, admin, employee, superAdmin } =
-    useUser(usersData, allUsers || [])
+    useUser(usersData, allUsers?.data || [])
 
   useEffect(() => {
     setCurrentPage(0)
     refetch()
   }, [itemsPerPage, searchQuery, sortBy, refetch])
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
-  }
-
-  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortBy(e.target.value)
-  }
 
   const handleItemsPerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setItemsPerPage(parseInt(e.target.value))
@@ -103,12 +94,8 @@ const Dashboard = () => {
       deleteUserMutation.mutate(
         { ...selectedUser },
         {
-          onSuccess: (response) => {
-            showToast({ status: 'success', title: response.message })
-          },
-          onError: (response) => {
-            showToast({ status: 'error', title: response.message })
-          }
+          onSuccess: (response) => showToast({ status: 'success', title: response.message }),
+          onError: (response) => showToast({ status: 'error', title: response.message })
         }
       )
     }
@@ -120,65 +107,49 @@ const Dashboard = () => {
       editUserMutation.mutate(
         { ...data },
         {
-          onSuccess: (response) => {
-            showToast({ status: 'success', title: response.message })
-            // getAllUser()
-          },
-          onError: (response) => {
-            showToast({ status: 'error', title: response.message })
-          }
+          onSuccess: (response) => showToast({ status: 'success', title: response.message }),
+          onError: (response) => showToast({ status: 'error', title: response.message })
         }
       )
     } else {
       addUserMutation.mutate(data, {
-        onSuccess: (response) => {
-          showToast({ status: 'success', title: response.message })
-          // getAllUser()
-        },
-        onError: (response) => {
-          showToast({ status: 'error', title: response.message })
-        }
+        onSuccess: (response) => showToast({ status: 'success', title: response.message }),
+        onError: (response) => showToast({ status: 'error', title: response.message })
       })
     }
     handleCloseUserModal()
   }
 
-  if (error || allUsersError) showToast({ status: 'error', title: error?.message || allUsersError?.message })
+  if (isError || allUsersIsError)
+    showToast({
+      status: 'error',
+      title: error?.message || allUsersError?.message || allUsersError?.message || 'Error fetching users'
+    })
 
   return (
     <Stack gap={6}>
       <Heading variant='primary' paddingLeft='13px'>
         Users
       </Heading>
-      <Flex gap={8} alignItems='center'>
-        <TextField
-          icon={<SearchIcon />}
-          variant='outline'
-          size='lg'
-          placeholder='Search'
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
+
+      <Filter>
         <Button display='flex' gap={2} onClick={onOpenUserModal}>
-          Add user
-          <PlusIcon />
+          Add user <PlusIcon />
         </Button>
-        <CustomSelect options={SORT_OPTION} placeholder='Sort by' onChange={handleSortChange} />
-        <FilterIcon />
-      </Flex>
+      </Filter>
 
       {isLoading ? (
         <div>Loading...</div>
       ) : (
         <Flex gap={4}>
-          <StatisticCard label='Users' value={allUsers?.length || 0} />
+          <StatisticCard label='Users' value={allUsers?.data?.length || 0} />
           <StatisticCard label='Super Admins' value={superAdmin.length} />
           <StatisticCard label='Admins' value={admin.length} />
           <StatisticCard label='Employees' value={employee.length} />
         </Flex>
       )}
 
-      {addUserMutation.isPending || editUserMutation.isPending || isFetchingNextPage || isFetching ? (
+      {addUserMutation.isPending || editUserMutation.isPending || isFetchingNextPage || isFetching || isLoading ? (
         <div>Loading...</div>
       ) : (
         <>
@@ -187,7 +158,7 @@ const Dashboard = () => {
           <Flex justifyContent='center'>
             <Pagination
               currentPage={currentPage + 1}
-              totalItems={allUsers?.length || 0}
+              totalItems={allUsers?.data?.length || 0}
               itemsPerPage={itemsPerPage}
               onPageChange={(page) => setCurrentPage(page - 1)}
               onItemsPerPageChange={handleItemsPerPageChange}
