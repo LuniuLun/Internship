f43f68084da5ba965d6f9cccafc4d69db2c0a942
DucVan <nguyenducvan260903@gmail.com>
2024-12-27 09:43:29 +0700

    [UPDATE] Remove FilterProvider from root component

--- react/Practice/practice-two/src/main.tsx
+++ react/Practice/practice-two/src/main.tsx
@@ -9,7 +9,6 @@ import { defaultTheme } from './styles'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { FilterProvider } from '@components/Filter/FilterProvider'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,11 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={defaultTheme} resetCSS>
-        <FilterProvider>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </FilterProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
