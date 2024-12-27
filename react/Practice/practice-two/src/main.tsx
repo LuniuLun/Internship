4a566671ef2ab97aff1372a68b0d31ab567d7c88
DucVan <nguyenducvan260903@gmail.com>
2024-12-27 16:39:06 +0700

    [UPDATE] Integrate SidebarProvider to manage sidebar state

--- react/Practice/practice-two/src/main.tsx
+++ react/Practice/practice-two/src/main.tsx
@@ -9,6 +9,7 @@ import { defaultTheme } from './styles'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SidebarProvider } from '@layout/components/Sidebar/SidebarProvider'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,7 +24,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <ChakraProvider theme={defaultTheme} resetCSS>
         <QueryClientProvider client={queryClient}>
-          <App />
+          <SidebarProvider>
+            <App />
+          </SidebarProvider>
         </QueryClientProvider>
       </ChakraProvider>
     </BrowserRouter>
