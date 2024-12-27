bd958077a7f0d621f1ed376f72cf330fb8f47610
DucVan <nguyenducvan260903@gmail.com>
2024-12-27 16:38:40 +0700

    [UPDATE] Enhance table layout with horizontal scrolling and fixed column width

--- react/Practice/practice-two/src/components/CustomTable/index.tsx
+++ react/Practice/practice-two/src/components/CustomTable/index.tsx
@@ -1,4 +1,17 @@
-import { Table, Thead, Tbody, Tr, Th, Td, IconButton, Flex, TableCaption, TableProps, Heading } from '@chakra-ui/react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  IconButton,
+  Flex,
+  TableCaption,
+  TableProps,
+  Heading,
+  Box
+} from '@chakra-ui/react'
 import { BinIcon, PenIcon } from '@assets/icons'
 import colors from '@styles/variables/colors'
 import CustomCell from './CustomCell'
@@ -28,91 +41,95 @@ const CustomTable = ({ title, data, onEdit, onDelete, ...props }: CustomTablePro
   const hasActions = Boolean(onEdit || onDelete)
 
   return (
-    <Table borderRadius='lg' {...props}>
-      {title && (
-        <TableCaption
-          placement='top'
-          marginTop={0}
-          padding={5}
-          textAlign='left'
-          color={colors.brand.blackTextPrimary}
-          fontSize='1.0625rem'
-          fontWeight='bold'
-          bgColor={colors.brand.white}
-        >
-          {title}
-        </TableCaption>
-      )}
-      <Thead>
-        <Tr bgColor={colors.brand.secondary} color={colors.brand.blackTextSecondary}>
-          {filteredHeaders.map((header, index) => (
-            <Th
-              key={header}
-              borderBottom={`2px solid ${colors.brand.secondary}`}
-              padding={5}
-              fontSize='md'
-              textTransform='capitalize'
-              textAlign={index === 0 ? 'left' : 'center'}
-            >
-              {header.replace(/([a-z])([A-Z])/g, '$1 $2')}
-            </Th>
-          ))}
-          {hasActions && (
-            <Th
-              textAlign='center'
-              w='100px'
-              padding={5}
-              fontSize='md'
-              textTransform='capitalize'
-              borderBottom={`2px solid ${colors.brand.secondary}`}
-            >
-              Action
-            </Th>
-          )}
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data.map((row, rowIndex) => (
-          <Tr key={rowIndex} fontSize='sm' color={colors.brand.blackTextPrimary}>
+    <Box overflowX='auto'>
+      <Table borderRadius='lg' {...props}>
+        {title && (
+          <TableCaption
+            placement='top'
+            marginTop={0}
+            padding={5}
+            textAlign='left'
+            color={colors.brand.blackTextPrimary}
+            fontSize='1.0625rem'
+            fontWeight='bold'
+            bgColor={colors.brand.white}
+          >
+            {title}
+          </TableCaption>
+        )}
+        <Thead>
+          <Tr bgColor={colors.brand.secondary} color={colors.brand.blackTextSecondary}>
             {filteredHeaders.map((header, index) => (
-              <Td
+              <Th
                 key={header}
                 borderBottom={`2px solid ${colors.brand.secondary}`}
+                padding={5}
+                minW='150px'
+                fontSize='md'
+                textTransform='capitalize'
                 textAlign={index === 0 ? 'left' : 'center'}
-                bgColor={colors.brand.white}
               >
-                {CustomCell({ header, row })}
-              </Td>
+                {header.replace(/([a-z])([A-Z])/g, '$1 $2')}
+              </Th>
             ))}
             {hasActions && (
-              <Td w='100px' borderBottom={`2px solid ${colors.brand.secondary}`} bgColor={colors.brand.white}>
-                <Flex gap={2}>
-                  {onEdit && (
-                    <IconButton
-                      aria-label='Edit'
-                      bgColor={colors.brand.white}
-                      icon={<PenIcon />}
-                      size='sm'
-                      onClick={() => onEdit && onEdit(row.id as string)}
-                    />
-                  )}
-                  {onDelete && (
-                    <IconButton
-                      aria-label='Delete'
-                      bgColor={colors.brand.white}
-                      icon={<BinIcon />}
-                      size='sm'
-                      colorScheme='red'
-                      onClick={() => onDelete && onDelete(row.id as string)}
-                    />
-                  )}
-                </Flex>
-              </Td>
+              <Th
+                textAlign='center'
+                minW='150px'
+                padding={5}
+                fontSize='md'
+                textTransform='capitalize'
+                borderBottom={`2px solid ${colors.brand.secondary}`}
+              >
+                Action
+              </Th>
             )}
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {data.map((row, rowIndex) => (
+            <Tr key={rowIndex} fontSize='sm' color={colors.brand.blackTextPrimary}>
+              {filteredHeaders.map((header, index) => (
+                <Td
+                  key={header}
+                  borderBottom={`2px solid ${colors.brand.secondary}`}
+                  minW='150px'
+                  textAlign={index === 0 ? 'left' : 'center'}
+                  bgColor={colors.brand.white}
+                >
+                  {CustomCell({ header, row })}
+                </Td>
+              ))}
+              {hasActions && (
+                <Td minW='150px' borderBottom={`2px solid ${colors.brand.secondary}`} bgColor={colors.brand.white}>
+                  <Flex gap={2} justifyContent='center'>
+                    {onEdit && (
+                      <IconButton
+                        aria-label='Edit'
+                        bgColor={colors.brand.white}
+                        icon={<PenIcon />}
+                        size='sm'
+                        onClick={() => onEdit && onEdit(row.id as string)}
+                      />
+                    )}
+                    {onDelete && (
+                      <IconButton
+                        aria-label='Delete'
+                        bgColor={colors.brand.white}
+                        icon={<BinIcon />}
+                        size='sm'
+                        colorScheme='red'
+                        onClick={() => onDelete && onDelete(row.id as string)}
+                      />
+                    )}
+                  </Flex>
+                </Td>
+              )}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
   )
 }
 
