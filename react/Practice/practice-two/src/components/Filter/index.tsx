a2727f1e2e315f3f18b3a93d5ec4393ffe710275
DucVan <nguyenducvan260903@gmail.com>
2025-01-01 17:42:18 +0700

    [UPDATE] Add loading state support to Filter component

--- react/Practice/practice-two/src/components/Filter/index.tsx
+++ react/Practice/practice-two/src/components/Filter/index.tsx
@@ -7,16 +7,25 @@ import { useFilterStore } from '@hooks/useFilterStore'
 
 interface FilterProps {
   children?: ReactNode
+  isLoaded?: boolean
 }
 
-const Filter = ({ children }: FilterProps) => {
+const Filter = ({ isLoaded = true, children }: FilterProps) => {
   const { searchQuery, sortBy, setSearchQuery, setSortBy } = useFilterStore()
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isLoaded) {
+      e.preventDefault()
+      return
+    }
     setSearchQuery(e.target.value)
   }
 
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (!isLoaded) {
+      e.preventDefault()
+      return
+    }
     setSortBy(e.target.value)
   }
 
@@ -29,6 +38,7 @@ const Filter = ({ children }: FilterProps) => {
         placeholder='Search'
         value={searchQuery}
         onChange={handleSearchChange}
+        isDisabled={!isLoaded}
       />
       <Flex gap={8} alignItems='center' w={{ base: '100%', md: 'unset' }}>
         {children}
@@ -38,6 +48,7 @@ const Filter = ({ children }: FilterProps) => {
           value={sortBy}
           onChange={handleSortChange}
           maxW={{ base: '100%', md: '150px' }}
+          isDisabled={!isLoaded}
         />
         <Box w='19px'>
           <FilterIcon />
