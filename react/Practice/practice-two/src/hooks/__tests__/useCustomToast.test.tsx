fabc7f423dc61b42ea76c76a00c5bc7db71f7587
DucVan <nguyenducvan260903@gmail.com>
2025-01-02 17:07:42 +0700

    [ADD] Add unit tests for `useCustomToast` hook

---
+++ react/Practice/practice-two/src/hooks/__tests__/useCustomToast.test.tsx
@@ -0,0 +1,47 @@
+import { useCustomToast } from '@hooks/useCustomToast'
+import { useToast } from '@chakra-ui/react'
+import { act, renderHook } from '@testing-library/react'
+
+jest.mock('@chakra-ui/react', () => ({
+  useToast: jest.fn()
+}))
+
+describe('useCustomToast', () => {
+  it('should call useToast with correct parameters', () => {
+    const showToast = jest.fn()
+    ;(useToast as jest.Mock).mockReturnValue(showToast)
+
+    const { result } = renderHook(() => useCustomToast())
+
+    act(() => {
+      result.current.showToast({ status: 'success', position: 'top', description: 'Test message' })
+    })
+
+    expect(showToast).toHaveBeenCalledWith({
+      status: 'success',
+      position: 'top',
+      isClosable: true,
+      variant: 'successCustom',
+      description: 'Test message'
+    })
+  })
+
+  it('should call useToast with default position when not provided', () => {
+    const showToast = jest.fn()
+    ;(useToast as jest.Mock).mockReturnValue(showToast) // Return the mock function as the result of useToast
+
+    const { result } = renderHook(() => useCustomToast())
+
+    act(() => {
+      result.current.showToast({ status: 'error', description: 'Error message' })
+    })
+
+    expect(showToast).toHaveBeenCalledWith({
+      status: 'error',
+      position: 'bottom',
+      isClosable: true,
+      variant: 'errorCustom',
+      description: 'Error message'
+    })
+  })
+})
