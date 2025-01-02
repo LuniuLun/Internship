a04ff2abfbe677de05a7872ffc9443cae615dc91
DucVan <nguyenducvan260903@gmail.com>
2025-01-02 17:10:47 +0700

    [UPDATE] Add custom scrollbar styles to global styles

--- react/Practice/practice-two/src/styles/globalStyles.tsx
+++ react/Practice/practice-two/src/styles/globalStyles.tsx
@@ -14,8 +14,29 @@ const globalStyles = {
         bg: 'brand.whiteBg'
         // color: mode('brand.blackTextPrimary', 'brand.white')(props),
         // bg: mode('brand.whiteBg', 'brand.blackBg')(props),
+      },
+
+      '::-webkit-scrollbar': {
+        width: '8px',
+        height: '10px',
+        backgroundColor: 'brand.secondary'
+      },
+
+      '::-webkit-scrollbar-thumb': {
+        borderRadius: '10px',
+        backgroundColor: 'brand.secondary'
+      },
+
+      '::-webkit-scrollbar-thumb:hover': {
+        backgroundColor: 'brand.blackTextSecondary'
+      },
+
+      '::-webkit-scrollbar-track': {
+        backgroundColor: 'brand.white',
+        borderRadius: '10px'
       }
     })
   }
 }
+
 export default globalStyles
