e58bcc666121b3b97432913165253bd9999d3e61
DucVan <nguyenducvan260903@gmail.com>
2025-01-03 11:44:57 +0700

    [ADD] Add test for useSidebar hook throwing error when not wrapped in SidebarProvider

---
+++ react/Practice/practice-two/src/hooks/__tests__/useSidebar.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { useSidebar } from '@hooks/useSidebar'
+
+test('useSidebar throws error when not wrapped in SidebarProvider', () => {
+  const TestComponent = () => {
+    useSidebar()
+    return <div>Test Component</div>
+  }
+
+  expect(() => render(<TestComponent />)).toThrow('useSidebar must be used within a SidebarProvider')
+})
