6f8d5b8dbd4caa32e7175fea9ca6c79596d14111
DucVan <nguyenducvan260903@gmail.com>
2025-01-08 17:15:48 +0700

    [ADD] add alias mapping for @utils in Jest configuration

--- react/Practice/practice-two/jest.config.ts
+++ react/Practice/practice-two/jest.config.ts
@@ -39,7 +39,8 @@ export default {
     '^@constants(.*)$': '<rootDir>/src/constants$1',
     '^@hooks(.*)$': '<rootDir>/src/hooks$1',
     '^@services(.*)$': '<rootDir>/src/services$1',
-    '^@contexts(.*)$': '<rootDir>/src/contexts$1'
+    '^@contexts(.*)$': '<rootDir>/src/contexts$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1'
   },
   moduleDirectories: ['node_modules', 'src']
 }
