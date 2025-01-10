99a07f41a7413ad68c88897ab0bbacebb24dd3c8
DucVan <nguyenducvan260903@gmail.com>
2025-01-10 15:44:18 +0700

    [UPDATE] Add snapshot test for active NavItem component

--- react/Practice/practice-two/src/components/NavItem/NavItem.test.tsx
+++ react/Practice/practice-two/src/components/NavItem/NavItem.test.tsx
@@ -1,8 +1,7 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { DashboardIcon } from '@assets/icons'
 import { MemoryRouter } from 'react-router-dom'
-import colors from '@styles/variables/colors'
 import NavItem from '@components/NavItem'
 
 describe('NavItem', () => {
@@ -13,33 +12,8 @@ describe('NavItem', () => {
       </MemoryRouter>
     )
 
-  it('should render the title and icon', () => {
-    const { container } = renderNavItem(false)
-    expect(screen.getByText('Test Title')).toBeInTheDocument()
-
-    const iconElement = container.querySelector('svg')
-    expect(iconElement).toBeInTheDocument()
-  })
-
-  it('should apply active styles when isActive is true', () => {
-    const { container } = renderNavItem(true)
-
-    const title = screen.getByText('Test Title')
-    const iconElement = container.querySelector('svg')
-
-    expect(title).toHaveStyle('font-weight: bold')
-    expect(title).toHaveStyle(`color: ${colors.brand.primary}`)
-    expect(iconElement).toHaveAttribute('fill', colors.brand.primary)
-  })
-
-  it('should apply default styles when isActive is false', () => {
-    const { container } = renderNavItem(false)
-
-    const title = screen.getByText('Test Title')
-    const iconElement = container.querySelector('svg')
-
-    expect(title).toHaveStyle('font-weight: normal')
-    expect(title).toHaveStyle(`color: ${colors.brand.blackTextTertiary}`)
-    expect(iconElement).toHaveAttribute('fill', colors.brand.blackTextTertiary)
+  it('matches the snapshot when isActive is true', () => {
+    const { asFragment } = renderNavItem(true)
+    expect(asFragment()).toMatchSnapshot()
   })
 })
