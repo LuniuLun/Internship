b4cdc873e5fe08d1e69c0d858653c04d6b89505a
DucVan <nguyenducvan260903@gmail.com>
2025-01-10 15:46:41 +0700

    [UPDATE] Remove compact test and add snapshot for UserCard component

--- react/Practice/practice-two/src/components/UserCard/UserCard.test.tsx
+++ react/Practice/practice-two/src/components/UserCard/UserCard.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import UserCard from '@components/UserCard'
 
 describe('UserCard', () => {
@@ -8,10 +8,12 @@ describe('UserCard', () => {
     return utils
   }
 
-  it('should render the user name and role correctly', () => {
-    renderUserCard('John Doe', 'Developer', 'https://cdn-icons-png.flaticon.com/512/3607/3607444.png')
-
-    expect(screen.getByText('John Doe')).toBeInTheDocument()
-    expect(screen.getByText('Developer')).toBeInTheDocument()
+  it('matches snapshot', () => {
+    const { asFragment } = renderUserCard(
+      'John Doe',
+      'Developer',
+      'https://cdn-icons-png.flaticon.com/512/3607/3607444.png'
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 })
