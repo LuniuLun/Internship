1bfec19c879f0f72dbc30683e6bbc6698c9ba562
DucVan <nguyenducvan260903@gmail.com>
2025-01-10 15:43:03 +0700

    [UPDATE] Simplify InfoGroup component tests with snapshot

--- react/Practice/practice-two/src/components/InfoGroup/InfoGroup.test.tsx
+++ react/Practice/practice-two/src/components/InfoGroup/InfoGroup.test.tsx
@@ -1,47 +1,14 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import InfoGroup from '@components/InfoGroup'
-import colors from '@styles/variables/colors'
 
 describe('InfoGroup', () => {
   it('renders the heading and description correctly', () => {
     const heading = 'Test Heading'
     const description = 'Test description for the InfoGroup component.'
 
-    render(<InfoGroup heading={heading} description={description} />)
+    const { asFragment } = render(<InfoGroup heading={heading} description={description} />)
 
-    const headingElement = screen.getByText(heading)
-    expect(headingElement).toBeInTheDocument()
-
-    const descriptionElement = screen.getByText(description)
-    expect(descriptionElement).toBeInTheDocument()
-  })
-
-  it('applies the correct font size and color for small size', () => {
-    const heading = 'Test Heading'
-    const description = 'Test description for the small size.'
-
-    render(<InfoGroup heading={heading} description={description} size='sm' />)
-
-    const headingElement = screen.getByText(heading)
-    expect(headingElement).toHaveStyle('font-size: 0.875rem')
-    expect(headingElement).toHaveStyle(`color: ${colors.brand.black}`)
-
-    const descriptionElement = screen.getByText(description)
-    expect(descriptionElement).toHaveStyle(`color: ${colors.brand.blackTextSecondary}`)
-  })
-
-  it('applies the correct font size and color for middle size', () => {
-    const heading = 'Test Heading'
-    const description = 'Test description for the middle size.'
-
-    render(<InfoGroup heading={heading} description={description} size='md' />)
-
-    const headingElement = screen.getByText(heading)
-    expect(headingElement).toHaveStyle('font-size: 1rem')
-    expect(headingElement).toHaveStyle(`color: ${colors.brand.black}`)
-
-    const descriptionElement = screen.getByText(description)
-    expect(descriptionElement).toHaveStyle(`color: ${colors.brand.blackTextTertiary}`)
+    expect(asFragment()).toMatchSnapshot()
   })
 })
