ebc23ddd1a2e81864613fda8c04257c2440bb237
DucVan <nguyenducvan260903@gmail.com>
2025-01-12 23:34:22 +0700

    [UPDATE] Enhance InfoGroup component: Add text truncation with ellipsis for heading and description

--- react/Practice/practice-two/src/components/InfoGroup/index.tsx
+++ react/Practice/practice-two/src/components/InfoGroup/index.tsx
@@ -12,13 +12,25 @@ interface IInfoGroupProps {
 const InfoGroup = ({ heading, description, size = 'sm' }: IInfoGroupProps) => {
   return (
     <Stack align='center' alignItems={'start'} gap={0}>
-      <Text fontWeight='500' fontSize={size === 'sm' ? '0.875rem' : '1rem'} color={colors.brand.black}>
+      <Text
+        fontWeight='500'
+        fontSize={size === 'sm' ? '0.875rem' : '1rem'}
+        color={colors.brand.black}
+        maxW='200px'
+        overflow='hidden'
+        textOverflow='ellipsis'
+        whiteSpace='nowrap'
+      >
         {heading}
       </Text>
       <Text
         fontWeight='400'
         fontSize='0.75rem'
         color={size === 'sm' ? colors.brand.blackTextSecondary : colors.brand.blackTextTertiary}
+        maxW='200px'
+        overflow='hidden'
+        textOverflow='ellipsis'
+        whiteSpace='nowrap'
       >
         {description}
       </Text>
