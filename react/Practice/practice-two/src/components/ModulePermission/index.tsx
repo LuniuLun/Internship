079214794d873d336e748aa43c211a77255a2c90
DucVan <nguyenducvan260903@gmail.com>
2025-01-12 23:08:46 +0700

    [UPDATE] Refactor ModulePermission: Move memoization outside the component definition

--- react/Practice/practice-two/src/components/ModulePermission/index.tsx
+++ react/Practice/practice-two/src/components/ModulePermission/index.tsx
@@ -1,8 +1,9 @@
 import CustomTable from '@components/CustomTable'
 import { MODULE_PERMISSION } from '@constants/option'
+import { memo } from 'react'
 
 const ModulePermission = () => {
   return <CustomTable data={MODULE_PERMISSION} marginTop={10} isLoaded={true} />
 }
 
-export default ModulePermission
+export default memo(ModulePermission)
