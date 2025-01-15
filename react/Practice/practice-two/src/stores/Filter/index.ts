1204b21a5f6d33c1b1ab50a717bdc416bf6860a0
DucVan <nguyenducvan260903@gmail.com>
2025-01-15 15:57:39 +0700

    feat(filterStore): add currentPage to filter state and setter method #15

--- react/Practice/practice-two/src/stores/Filter/index.ts
+++ react/Practice/practice-two/src/stores/Filter/index.ts
@@ -4,6 +4,8 @@ interface FilterState {
   searchQuery: string
   sortBy: string
   itemsPerPage: number
+  currentPage: number
+  setCurrentPage: (currentPage: number) => void
   setItemsPerPage: (itemsPerPage: number) => void
   setSearchQuery: (query: string) => void
   setSortBy: (sort: string) => void
@@ -13,6 +15,8 @@ const filterStore = create<FilterState>((set) => ({
   searchQuery: '',
   sortBy: '',
   itemsPerPage: 5,
+  currentPage: 0,
+  setCurrentPage: (currentPage: number) => set(() => ({ currentPage: currentPage })),
   setSearchQuery: (query: string) => set(() => ({ searchQuery: query })),
   setSortBy: (sort: string) => set(() => ({ sortBy: sort })),
   setItemsPerPage: (itemsPerPage: number) => set(() => ({ itemsPerPage }))
