00ae4d7efea4c9f8c8d64bb268c0063c2223a083
DucVan <nguyenducvan260903@gmail.com>
2025-01-16 16:55:33 +0700

    test(filterStore): updates conditional for searchQuery, itemsPerPage, and sortBy in filterStore #19

--- react/Practice/practice-two/src/stores/Filter/filterStore.test.tsx
+++ react/Practice/practice-two/src/stores/Filter/filterStore.test.tsx
@@ -7,12 +7,13 @@ describe('filterStore', () => {
       filterStore.setState({
         searchQuery: '',
         sortBy: '',
-        itemsPerPage: 5
+        itemsPerPage: 5,
+        currentPage: 0
       })
     })
   })
 
-  it('should update searchQuery when setSearchQuery is called', () => {
+  it('should update searchQuery when setSearchQuery is called with a new value', () => {
     const { setSearchQuery } = filterStore.getState()
 
     act(() => {
@@ -23,7 +24,18 @@ describe('filterStore', () => {
     expect(searchQuery).toBe('Test Query')
   })
 
-  it('should update itemsPerPage when setItemsPerPage is called', () => {
+  it('should not update searchQuery if the same value is passed to setSearchQuery', () => {
+    const { setSearchQuery } = filterStore.getState()
+
+    act(() => {
+      setSearchQuery('')
+    })
+
+    const { searchQuery } = filterStore.getState()
+    expect(searchQuery).toBe('')
+  })
+
+  it('should update itemsPerPage when setItemsPerPage is called with a new value', () => {
     const { setItemsPerPage } = filterStore.getState()
 
     act(() => {
@@ -34,7 +46,18 @@ describe('filterStore', () => {
     expect(itemsPerPage).toBe(10)
   })
 
-  it('should update sortBy when setSortBy is called', () => {
+  it('should not update itemsPerPage if the same value is passed to setItemsPerPage', () => {
+    const { setItemsPerPage } = filterStore.getState()
+
+    act(() => {
+      setItemsPerPage(5)
+    })
+
+    const { itemsPerPage } = filterStore.getState()
+    expect(itemsPerPage).toBe(5)
+  })
+
+  it('should update sortBy when setSortBy is called with a new value', () => {
     const { setSortBy } = filterStore.getState()
 
     act(() => {
@@ -43,5 +66,16 @@ describe('filterStore', () => {
 
     const { sortBy } = filterStore.getState()
     expect(sortBy).toBe('name')
+  })
+
+  it('should not update sortBy if the same value is passed to setSortBy', () => {
+    const { setSortBy } = filterStore.getState()
+
+    act(() => {
+      setSortBy('')
+    })
+
+    const { sortBy } = filterStore.getState()
+    expect(sortBy).toBe('')
   })
 })
