090c42997a175c695b669bacbebbdda1ebad41a0
DucVan <nguyenducvan260903@gmail.com>
2025-01-17 11:40:22 +0700

    feat: add ProtectedRoute for user role-based access control #22

--- react/Practice/practice-two/src/App.tsx
+++ react/Practice/practice-two/src/App.tsx
@@ -2,13 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import DefaultLayout from '@layout'
 import Dashboard from '@pages/Dashboard'
 import Users from '@pages/Users'
+import ProtectedRoute from './routes/ProtectedRoute'
+import { authStore } from '@stores'
 
 function App() {
+  const { user } = authStore()
+
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route index element={<Dashboard />} />
-        <Route path='users' element={<Users />} />
+        <Route
+          element={<ProtectedRoute isAllowed={!!user} userRoles={user?.roles} requiredRole='admin' redirectPath='/' />}
+        >
+          <Route path='users' element={<Users />} />
+        </Route>
       </Route>
     </Routes>
   )
