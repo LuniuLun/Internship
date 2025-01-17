a423df263dc167d1b15565e669d800c5a1da74cb
DucVan <nguyenducvan260903@gmail.com>
2025-01-17 17:32:30 +0700

    feat: wrap DefaultLayout with ErrorBoundary for error handling #23

--- react/Practice/practice-two/src/layout/index.tsx
+++ react/Practice/practice-two/src/layout/index.tsx
@@ -2,16 +2,19 @@ import { Outlet } from 'react-router-dom'
 import { Flex, Stack } from '@chakra-ui/react'
 import Header from '@layout/components/Header'
 import Sidebar from '@layout/components/Sidebar'
+import { ErrorBoundary } from '@components'
 
 const DefaultLayout = () => {
   return (
-    <Flex>
-      <Sidebar />
-      <Stack w={{ base: '100%', xl: 'calc(100% - 254px)' }} px={6} paddingBottom={6} bgColor='brand.grey'>
-        <Header />
-        <Outlet />
-      </Stack>
-    </Flex>
+    <ErrorBoundary>
+      <Flex>
+        <Sidebar />
+        <Stack w={{ base: '100%', xl: 'calc(100% - 254px)' }} px={6} paddingBottom={6} bgColor='brand.grey'>
+          <Header />
+          <Outlet />
+        </Stack>
+      </Flex>
+    </ErrorBoundary>
   )
 }
 
